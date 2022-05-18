cd356ccafddfc24254786e0488fe6e6eb4b60c81
Ray Rodriguez <RRodriguez4@Paylocity.com>
2022-05-18 13:31:20 -0500

    Initial Commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cryx6nekpbzo.us-east-2.rds.amazonaws.com"
+customuser = "admin"
+custompass = "gdYHvQgKoUQwYKtatH1N"
+customdb = "employee"
+custombucket = "pcty-employee"
+customregion = "us-east-2"
